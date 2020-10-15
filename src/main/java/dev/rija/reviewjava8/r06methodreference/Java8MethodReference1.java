ee114bf8cce1cd36bad6c0900f6c5396449c9be3
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-10-15 14:01:27 +0200

    method reference - 1. static method

---
+++ src/main/java/dev/rija/reviewjava8/r06methodreference/Java8MethodReference1.java
@@ -0,0 +1,49 @@
+package dev.rija.reviewjava8.r06methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * the double colon (::) operator is called method references.
+ * another way call to an existing method.
+ *
+ * There are 4 kinds of method references:
+ *
+ * Reference to a static method ClassName::staticMethodName
+ * Reference to an instance method of a particular object Object::instanceMethodName
+ * Reference to an instance method of an arbitrary object of a particular type ContainingType::methodName–
+ * Reference to a constructor ClassName::new
+ *
+ * 1. Static method
+ * lambda expression
+ * (args) -> ClassName.staticMethodName(args)
+ * =>
+ * method reference
+ * ClassName::staticMethodName
+ */
+public class Java8MethodReference1 {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("A", "B", "C");
+		
+		// anonymous class
+		list.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				SimplePrinter.print(s);
+			}
+		});
+		
+		// lambda expression
+		list.forEach(x -> SimplePrinter.print(x));
+		
+		// method reference
+		list.forEach(SimplePrinter::print);
+	}
+}
+
+class SimplePrinter {
+	public static void print(String str) {
+		System.out.println(str);
+	}
+}
---
+++ src/main/java/dev/rija/reviewjava8/r06methodreference/Java8MethodReference3.java
@@ -0,0 +1,46 @@
+package dev.rija.reviewjava8.r06methodreference;
+
+import java.util.function.BiFunction;
+
+/**
+ * 1. Static method
+ * lambda expression
+ * (args) -> ClassName.staticMethodName(args)
+ * =>
+ * method reference
+ * ClassName::staticMethodName
+ *
+ * This example joins two Integer and returns a String.
+ * It passes a method reference static method IntegerUtils::join as an argument into another method that accepts a BiFunction.
+ */
+public class Java8MethodReference3 {
+	public static void main(String[] args) {
+		// anonymous class
+		String result1 = playTwoArguments(1, 2, new BiFunction<Integer, Integer, String>() {
+			@Override
+			public String apply(Integer i1, Integer i2) {
+				return IntegerUtils.join(i1, i2);
+			}
+		});
+		System.out.println(result1);
+		
+		// lambda
+		String result2 = playTwoArguments(1, 2, (a, b) -> IntegerUtils.join(a, b));
+		System.out.println(result2);
+		
+		// method reference
+		String result3 = playTwoArguments(1, 2, IntegerUtils::join);
+		System.out.println(result3);
+	}
+	
+	private static <R> R playTwoArguments(Integer x1, Integer x2,
+										  BiFunction<Integer, Integer, R> func) {
+		return func.apply(x1, x2);
+	}
+}
+
+class IntegerUtils {
+	public static String join(Integer a, Integer b) {
+		return String.valueOf(a + b);
+	}
+}
